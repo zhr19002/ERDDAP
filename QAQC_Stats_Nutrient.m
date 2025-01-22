06abca36c7186a085f9e9e2b0b601f6807ba8a4c
zhr19002 <zrenseu@gmail.com>
2025-01-22 16:58:46 -0500

    Initial commit

--- QAQC_Stats_Nutrient.m
+++ QAQC_Stats_Nutrient.m
@@ -1,5 +1,5 @@
 % 
-% Generate a statistics table of station climatology QAQC results
+% Generate a statistics table of station nutrient QAQC results
 % 
 
 clc; clear;
@@ -12,20 +12,22 @@ connQ = postgresql(username,password,'Server','merlin.dms.uconn.edu', ...
      'DatabaseName','stationQAQC','PortNumber',5432);
 
 % Extract table from PostgreSQL
-dT = sqlread(connQ, ['"DEEP_' Astn '_WQ_QAQC"']);
+dT = sqlread(connQ, ['"DEEP_' Astn '_Nutrient_QAQC"']);
 close(connQ);
 
 % Statistics of QAQC results
-stats_tbl = table((1:4)','VariableNames',{'Flag'});
-for av = {'T','S','DO','P','C','rho','pH','DOsat','PAR','Corrected_Chl'}
-    tmp = tabulate(dT.([av{1} '_Q']));
-    for n = 1:4
+stats_tbl = table((0:4)','VariableNames',{'Flag'});
+for av = {'BIOSI-LC','DIP','DOC','NH#-LC','NOX-LC','PC', ...
+          'PN','PP-LC','SIO2-LC','TDN-LC','TDP','TSS'}
+    iu = strcmp(dT.Parameter, av{1});
+    tmp = tabulate(dT.Result_Q(iu));
+    for n = 0:4
         if ~ismember(n,tmp(:,1))
             tmp = [tmp; [n 0 0]];
         end
     end
     tmp = sortrows(tmp);
-    av_count = arrayfun(@(i) sprintf('%d (%.2f%%)', tmp(i,2), tmp(i,3)), 1:4, 'UniformOutput', false);
+    av_count = arrayfun(@(i) sprintf('%d (%.2f%%)', tmp(i,2), tmp(i,3)), 1:5, 'UniformOutput', false);
     stats_tbl.(av{1}) = av_count';
 end
 disp(stats_tbl);
